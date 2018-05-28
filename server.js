441315a8fd497a9a01d86b1bf72c8c1746b7c55f
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-28 12:37:45 -0600

    Add tests directory, update scritps in package json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
