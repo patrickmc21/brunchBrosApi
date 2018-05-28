c6f857b17dda33928dbaf2790283579b1aea5edf
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-28 11:55:46 -0600

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,39 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/brunchbrosdb',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/brunchbrosdb_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/prod'
+    },
+    useNullAsDefault: true
+  }
+
+};
