312412dca1c8dac5b6ba7e8754acfbd18a682274
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-28 14:46:45 -0600

    Configure database in routes, add users get route

--- routes/maps.js
+++ routes/maps.js
@@ -1,4 +1,7 @@
 const routes = require('express').Router();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const db = require('knex')(configuration);
 
 routes.get('/:userId', (req, res) => {
   
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 const routes = require('express').Router();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const db = require('knex')(configuration);
 
 routes.get('/', (req, res) => {
-  // use a query param?
-  // return user
+  const { email } = req.query;
+
+  db('users').select().where('email', email)
+    .then((user) => {
+      res.status(200).json(user[0]);
+    })
+    .catch(error => {
+      res.status(404).json({message: 'User not found'});
+    })
 });
 
 routes.post('/', (req, res) => {
