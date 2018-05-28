899f1cb8b2b24a0b5db4b28c54148c94e17df75f
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-28 12:14:52 -0600

    Route frameworks for maps, pins, and posts

--- routes/maps.js
+++ routes/maps.js
@@ -1,1 +1,19 @@
 const routes = require('express').Router();
+
+routes.get('/:userId', (req, res) => {
+  
+});
+
+routes.post('/', (req, res) => {
+
+});
+
+routes.put('/:id', (req, res) => {
+
+});
+
+routes.delete('/:id', (req, res) => {
+
+});
+
+module.exports = routes;
