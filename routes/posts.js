c5bd9501a3e64ae0af95adc270d60baaa428ad9e
Matt Walker <mttwlkr@gmail.com>
2018-05-28 16:02:49 -0600

    Initial commit

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,15 @@ const configuration = require('../knexfile')[environment];
 const db = require('knex')(configuration);
 
 routes.get('/:pinId', (req, res) => {
-  
+  const { id } = req.params;
+
+  db('posts').select().where('ID', id)
+    .then((pin) => {
+      res.status(200).json(pin[0]);
+    })
+    .catch(error => {
+      res.status(404).json({error, message: 'Post not found'})
+    })
 });
 
 routes.post('/', (req, res) => {
