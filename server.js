6a40201c1439e07dbf3ecffa5e87d53a25288ff6
Matt Walker <mttwlkr@gmail.com>
2018-06-05 15:46:05 -0600

    Add and install cors

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const app = express();
+const cors = require('cors')
 
+app.use(cors());
 app.set('port', process.env.PORT || 3001);
 app.use(bodyParser.json({ strict: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
